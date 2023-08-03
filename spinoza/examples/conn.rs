008e925083937363b286e9e28ad4459a724e2ef7
Saveliy Yusufov <sy2685@columbia.edu>
2023-08-03 09:59:38 -0400

    Updates

--- spinoza/examples/conn.rs
+++ spinoza/examples/conn.rs
@@ -17,34 +17,10 @@ fn main() {
     let mut client1 = Node::new(DRIVER_ADDR, NODE1_ADDR);
     let mut client2 = Node::new(DRIVER_ADDR, NODE2_ADDR);
     let mut client3 = Node::new(DRIVER_ADDR, NODE3_ADDR);
-    let mut client4 = Node::new(DRIVER_ADDR, NODE4_ADDR);
 
     std::thread::scope(|s| {
         s.spawn(|| {
             driver.run();
         });
-
-        for i in 0..3 {
-            s.spawn(move || {
-                let mut client = Node::new(ADDR);
-                let (buf, data_type) = client.recv();
-
-                match data_type {
-                    DataType::Amplitude => {
-                        let mut amplitude = amp_from_msg(&buf);
-                        println!("client {i} recvd amplitude: {:?}", amplitude);
-                        amplitude.re *= i as Float;
-                        amplitude.im += i as Float;
-
-                        println!(
-                            "client {i} now sending back updated amplitude: {:?}\n",
-                            amplitude
-                        );
-                        client.send(&amp_to_msg(amplitude), DataType::Amplitude);
-                    }
-                    _ => unimplemented!(),
-                }
-            });
-        }
     });
 }

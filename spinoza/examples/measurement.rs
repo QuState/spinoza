8fe5a7a920da38512e84a720e479e6273cc1a1a2
Saveliy Yusufov <sy2685@columbia.edu>
2023-10-13 15:19:06 -0400

    Add measurement example

---
+++ spinoza/examples/measurement.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+use spinoza::{
+    config::{Config, QSArgs},
+    core::{State, CONFIG},
+    gates::{apply, Gate},
+    measurement::measure_qubit,
+    utils::{pretty_print_int, to_table},
+};
+
+fn h(n: usize, show_results: bool) {
+    let now = std::time::Instant::now();
+    let mut state = State::new(n);
+
+    for t in 0..n {
+        apply(Gate::H, &mut state, t);
+    }
+
+    let elapsed = now.elapsed().as_micros();
+    println!("{}", pretty_print_int(elapsed));
+
+    let now = std::time::Instant::now();
+    measure_qubit(&mut state, n - 1, Some(1));
+    let elapsed = now.elapsed().as_micros();
+    println!("{}", pretty_print_int(elapsed));
+}
+
+fn main() {
+    let args = QSArgs::parse();
+    let config = Config::from_cli(args);
+    CONFIG.set(config).unwrap();
+    h(config.qubits.into(), config.print);
+}
